03fb6f3622d8f9b1c8a9dc946084e83b4c9c78d1
Lucas <lucasrian.dev@gmail.com>
2026-04-09 15:19:35 -0300

    lista de ordenacao

--- Ativ2/script.js
+++ Ativ2/script.js
@@ -8,4 +8,18 @@ function buscarMenor(arr) {
       menorIndice = i;
     }
   }
+  return menorIndice;
 }
+
+function ordenacaoPorSelecao(arr) {
+  let novoArr = [];
+  let tamanhoOriginal = arr.length;
+
+  for (let i = 0; i < tamanhoOriginal; i++) {
+    let menor = buscarMenor(arr);
+    novoArr.push(arr.splice(menor, 1)[0]);
+  }
+  return novoArr;
+}
+
+console.log(ordenacaoPorSelecao([5, 3, 6, 2, 10]));
